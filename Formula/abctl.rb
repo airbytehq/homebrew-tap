91a0676320b6bfd0277beaa726f5de8491d77ad7
Cole Snodgrass <cole@airbyte.io>
2024-04-29 18:20:50 -0700

    update abctl forumula to v0.2.2 (#4)

--- Formula/abctl.rb
+++ Formula/abctl.rb
@@ -1,8 +1,8 @@
 class Abctl < Formula
   desc "Airbyte's command-line tool for running Airbyte locally"
   homepage "https://github.com/airbytehq/abctl"
-  url "https://github.com/airbytehq/abctl/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "704070b22fd94a1d9d6ab101fe11638d692e5a7f3771809ca9936613a4c187e3"
+  url "https://github.com/airbytehq/abctl/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "6013da1ed0c43f27d02d9f2a90e8091f9ab2517d6efe0c56a1978bc80f13ce3e"
   license "MIT"
 
   livecheck do
@@ -13,12 +13,12 @@ class Abctl < Formula
   depends_on "go" => :build
 
   def install
-    ENV["ABCTL_VERSION"] = "v0.2.1"
+    ENV["ABCTL_VERSION"] = "v0.2.2"
     system "make", "build"
     bin.install "build/abctl"
   end
 
   test do
-    assert_match "version: v0.2.1", shell_output("#{bin}/abctl version").strip
+    assert_match "version: v0.2.2", shell_output("#{bin}/abctl version").strip
   end
 end
