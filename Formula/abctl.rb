ad8eee8df3113af521f5f1aa4ae7a5b1dec90446
Cole Snodgrass <cole@airbyte.io>
2024-05-13 15:50:29 -0700

    upgrade to v0.4.0

--- Formula/abctl.rb
+++ Formula/abctl.rb
@@ -1,8 +1,8 @@
 class Abctl < Formula
   desc "Airbyte's command-line tool for running Airbyte locally"
   homepage "https://github.com/airbytehq/abctl"
-  url "https://github.com/airbytehq/abctl/archive/refs/tags/v0.3.2.tar.gz"
-  sha256 "52989fa9abff70ebdeb96a2094c43badbfac0249080a71c60ffa0a9d47bfe8bd"
+  url "https://github.com/airbytehq/abctl/archive/refs/tags/v0.4.0.tar.gz"
+  sha256 "aca6666f18a1f8864e2150f848d2ddfa6ebbbdf8301139b3ce4098567387a1b8"
   license "MIT"
 
   livecheck do
@@ -13,12 +13,12 @@ class Abctl < Formula
   depends_on "go" => :build
 
   def install
-    ENV["ABCTL_VERSION"] = "v0.3.2"
+    ENV["ABCTL_VERSION"] = "v0.4.0"
     system "make", "build"
     bin.install "build/abctl"
   end
 
   test do
-    assert_match "version: v0.3.2", shell_output("#{bin}/abctl version").strip
+    assert_match "version: v0.4.0", shell_output("#{bin}/abctl version").strip
   end
 end
