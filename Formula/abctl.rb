5fdd4d424b9dc130689809045c385c29b055849d
airbyte-prodeng <production-engineering-ops@airbyte.io>
2025-08-14 22:19:56 +0000

    chore: update abctl to v0.30.0

--- Formula/abctl.rb
+++ Formula/abctl.rb
@@ -1,5 +1,5 @@
 class Abctl < Formula
-  ABCTL_VERSION = "v0.29.0".freeze
+  ABCTL_VERSION = "v0.30.0".freeze
 
   desc "Airbyte's command-line tool for running Airbyte locally"
   homepage "https://github.com/airbytehq/abctl"
